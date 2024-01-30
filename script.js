01fee3fbcf577179276e090b0e88551463dd0ae4
lsem93 <152432044+lsem93@users.noreply.github.com>
2024-01-30 17:21:07 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('subscribeForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    // Display an alert message
+    alert('You are subscribed!');
+});
